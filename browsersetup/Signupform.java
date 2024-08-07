61bbaccb4f1a0b2285ff267d02a2d33093181e65
charu664 <157209830+charu664@users.noreply.github.com>
2024-08-07 12:26:14 +0530

    Add files via upload

--- browsersetup/Signupform.java
+++ browsersetup/Signupform.java
@@ -1,84 +1,88 @@
 package browsersetup;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import browserPage.signuppage;
-import org.testng.annotations.Test;
-public class Signupform {
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import java.util.concurrent.TimeUnit;
+public class Signupform    {
     WebDriver driver;
-    @Test
-    public void signup() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\ADMIN\\Downloads\\chromedriver-win64\\chromedriver.exe");
+    signuppage signuppage;
+    @BeforeMethod
+    public void signup() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.MINUTES);
-        driver.get("https://portal.thecoremarkets.com/user/login");
-
-        signuppage signup = new signuppage(driver);
-//        signup.getStared();
-//        signup.enterfirstname();
-//        signup.enterlastname();
-//        signup.enterEmail();
-//        signup.enternumber();
-//        signup.country();
-//        signup.clickcountry();
-//        signup.enterPasswrod();
-//        signup.enterpromo();
-//        signup.clickcheckbox();
-//        signup.button();
-          signup.useremail();
-          signup.userpassword();
-          signup.signin();
-          signup.dashboard();
-          signup.Profilesetting();
-          signup.mydetails();
-//        signup.editbutton();
-//        signup.editname();
-//        signup.editlastname();
-//        signup.editemail();
-//        signup.editpass();
-//        signup.editphone();
-//        signup.submitbutton();
-//        signup.cancelbutton();
-          signup.backbutton();
-          signup.verification();
-//        signup.flip();
-//        signup.upload();
-//        signup.submit();
-          signup.BACKBUTTON();
-//        signup.Logoutbutton();
-//        signup.logout();
-//        signup.Useremail1();
-//        signup.Userpassword1();
-//        signup.Signin1();
-//        signup.Useradmin();
-//        signup.Userverification();
-//        signup.Adminlog();
-//        signup.Adminlogo();
-//        signup.Useremail2();
-//        signup.Userpassword2();
-//        signup.Signin2();
-//        signup.Profilesetting2();
-          signup.changepss();
-//        signup.Oldpass();
-//        signup.newpass();
-//        signup.Confipass();
-//        signup.updatepass();
-          signup.Backbuttonchnagepass();
-//        signup.Transaction();
-//        signup.Deposit();
-//        signup.Wallet();
-//        signup.item();
-//        signup.itemselect();
-//        signup.Selectbank();
-//        signup.amount();
-//        signup.paybutton();
-//        signup.Backb();
-//        signup.Mt5account();
-
-
-
-
+        signuppage = new signuppage(driver);
+        driver.get("https://backend.crownstonecrm.com/user/login");
     }
-}
+    @Test
+    public void signupTest() {
+        signuppage.getStared();
+        signuppage.enterfirstname();
+        signuppage.enterlastname();
+        signuppage.enterEmail();
+        signuppage.enternumber();
+        signuppage.country();
+        signuppage.clickcountry();
+        signuppage.enterPasswrod();
+        signuppage.enterpromo();
+        signuppage.clickcheckbox();
+//        signuppage.button();
+//        signuppage.useremail();
+//        signuppage.userpassword();
+//        signuppage.signin();
+//        signuppage.dashboard();
+//        signuppage.Profilesetting();
+//        signuppage.mydetails();
+//        signuppage.editbutton();
+//        signuppage.editname();
+//        signuppage.editlastname();
+//        signuppage.editemail();
+//        signuppage.editpass();
+//        signuppage.editphone();
+//        signuppage.submitbutton();
+//        signuppage.cancelbutton();
+//        signuppage.backbutton();
+//        signuppage.verification();
+//        signuppage.flip();
+//        signuppage.upload();
+//        signuppage.submit();
+//        signuppage.BACKBUTTON();
+//        signuppage.Logoutbutton();
+//        signuppage.logout();
+//        signuppage.Useremail();
+//        signuppage.userpassword1();
+//        signuppage.Signin1();
+//        signuppage.Useradmin();
+//        signuppage.Userverification();
+//        signuppage.Adminlog();
+//        signuppage.Adminlogo();
+//        signuppage.Useremail2();
+//        signuppage.Userpassword2();
+//        signuppage.Signin2();
+//        signuppage.Profilesetting2();
+//        signuppage.changepss();
+//        signuppage.Oldpass();
+//        signuppage.newpass();
+//        signuppage.Confipass();
+//        signuppage.updatepass();
+//        signuppage.Backbuttonchnagepass();
+//        signuppage.Transaction();
+//        signuppage.Deposit();
+//        signuppage.Wallet();
+//        signuppage.item();
+//        signuppage.itemselect();
+//        signuppage.selectbank();
+//        signuppage.amount();
+//        signuppage.paybutton();
+//        signuppage.Backb();
+//        signuppage.Mt5account();
+//        }
+//  }
+    }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }}
+
 
